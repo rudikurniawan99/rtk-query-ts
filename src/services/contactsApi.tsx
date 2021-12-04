181b8bb1b28963b1cc322103db02579874b34a46
rudikurniawan99 <rudikurniawan501@gmail.com>
2021-12-04 14:40:25 +0700

    add update,and delete contact hooks

--- src/services/contactsApi.tsx
+++ src/services/contactsApi.tsx
@@ -5,13 +5,34 @@ export const contactsApi = createApi({
   reducerPath: 'contactsApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3002' }),
   endpoints: (builder) => ({
+    //hook_name: builder.(query/mutation)<outputType, inputType>
     contacts: builder.query<Contact[], void>({
-      query: ()=> '/contacts'
+      query: () => '/contacts'
     }),
     contact: builder.query<Contact, string>({
-      query: (id) => `/contact/${id}`
+      query: (id) => `/contacts/${id}`
+    }),
+    addContact: builder.mutation<void, Contact>({
+      query: (contact) => ({
+        url: '/contacts',
+        method: 'POST',
+        body: contact,
+      })
+    }),
+    updateContact: builder.mutation<void, Contact>({
+      query: ({id, ...rest}) => ({
+        url: `/contacts/${id}`,
+        method: 'PUT',
+        body: rest 
+      })
+    }),
+    deleteContact: builder.mutation<void, string>({
+      query: (id) => ({
+        url: `/contacts/${id}`,
+        method: 'DELETE',
+      })
     })
   }) 
 })
 
-export const { useContactsQuery, useContactQuery } = contactsApi
+export const { useContactsQuery, useContactQuery, useAddContactMutation, useDeleteContactMutation } = contactsApi
